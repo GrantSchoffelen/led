df176c7fd1822f8d32cc1f6dcba0a6f15cc37683
Macbook pro <Home@Macbooks-MacBook-Pro.local>
2017-05-08 00:11:41 -0400

    update

--- utils/news.js
+++ utils/news.js
@@ -17,7 +17,7 @@ module.exports = {
             var cmd = `sudo ./runtext.py --led-no-hardware-pulse LED_NO_HARDWARE_PULSE -c 2 -t '${headlines}'`;
             console.log(cmd)
             exec('cd utils/matrix/python/samples/', function(error, stdout, stderr) {
-                exec('cmd', function(error, stdout, stderr) {
+                exec(cmd, function(error, stdout, stderr) {
                     if (error) {
                       console.error(`exec error: ${error}`);
                       return;
