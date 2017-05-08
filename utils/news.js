a68efde3433dfcfa7763b1dac5286c1253e2970f
Macbook pro <Home@Macbooks-MacBook-Pro.local>
2017-05-07 22:56:59 -0400

    sub module

--- utils/news.js
+++ utils/news.js
@@ -3,19 +3,15 @@ config = require('../config');
 
 
 module.exports = {
-
-getNewsHeadlines: (source = 'techcrunch')=>{
-    http.get(`https://newsapi.org/v1/articles`
-    ).query({ apiKey: config.newsApiKey, source: source})
-    .end((err, res)=>{
-        const resObj = JSON.parse(res.text)
-        const articles = resObj.articles;
-        articles.forEach((article)=>{
-            console.log(article.title)
+    getNewsHeadlines: (source = 'techcrunch')=>{
+        http.get(`https://newsapi.org/v1/articles`
+        ).query({ apiKey: config.newsApiKey, source: source})
+        .end((err, res)=>{
+            const resObj = JSON.parse(res.text)
+            const articles = resObj.articles;
+            articles.forEach((article)=>{
+                console.log(article.title)
+            })
         })
-    })
-}
-
-
-
+    }
 }
