8675b9235b089cf653677f9e5271d6520c218d12
Macbook pro <Home@Macbooks-MacBook-Pro.local>
2017-05-17 22:26:49 -0400

    config file fix

--- utils/clock.js
+++ utils/clock.js
@@ -1,9 +1,10 @@
 var exec = require('child_process').exec;
+var config = require('../config');
 
 module.exports = {
     startClock: function(){
         var cmd = `sudo ./time.py --led-no-hardware-pulse LED_NO_HARDWARE_PULSE -c 2`;
-        exec(cmd, {cwd:'/home/pi/led/utils/matrix/python/samples'}, function(error, stdout, stderr) {
+        exec(cmd, {cwd: config.pathToMatrixLib + '/python/samples'}, function(error, stdout, stderr) {
             if (error) {
               console.error(`exec error: ${error}`);
               return;
--- utils/news.js
+++ utils/news.js
@@ -16,7 +16,7 @@ module.exports = {
             })
 
             var cmd = `sudo ./runtext.py --led-no-hardware-pulse LED_NO_HARDWARE_PULSE -c 2 -t '${headlines}'`;
-            exec(cmd, {cwd:'/home/pi/led/utils/matrix/python/samples'}, function(error, stdout, stderr) {
+            exec(cmd, {cwd: config.pathToMatrixLib + '/python/samples'}, function(error, stdout, stderr) {
                 if (error) {
                   console.error(`exec error: ${error}`);
                   return;
