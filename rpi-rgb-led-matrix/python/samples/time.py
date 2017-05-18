386bbde9efa1e05a10d75fddceda441de2d321f2
Macbook pro <Home@Macbooks-MacBook-Pro.local>
2017-05-17 22:12:10 -0400

    update time

--- rpi-rgb-led-matrix/python/samples/time.py
+++ rpi-rgb-led-matrix/python/samples/time.py
@@ -1,26 +1,26 @@
+
 #!/usr/bin/env python
 from samplebase import SampleBase
 from rgbmatrix import graphics
 from datetime import datetime
 from pytz import timezone
 import time
-eastern = timezone('EST')
-
+eastern = timezone('US/Eastern')
 class GraphicsTest(SampleBase):
     def __init__(self, *args, **kwargs):
-	super(GraphicsTest, self).__init__(*args, **kwargs)
+        super(GraphicsTest, self).__init__(*args, **kwargs)
 
     def run(self):
         canvas = self.matrix
         font = graphics.Font()
-        font.LoadFont("../../fonts/5x7.bdf")
+        font.LoadFont("../../fonts/4x6.bdf")
         color = graphics.Color(0, 255, 255)
-	
-	while True:
-	    self.matrix.Clear()
-	    txtTime = datetime.now(eastern).strftime('%l:%M:%S%p') 
-	    graphics.DrawText(canvas, font, 0, 6, color, txtTime)
-	    time.sleep(1)
+
+        while True:
+            self.matrix.Clear()
+            txtTime = datetime.now(eastern).strftime('%l:%M:%Sp')
+            graphics.DrawText(canvas, font, 0, 6, color, txtTime)
+            time.sleep(1)
 
 # Main function
 if __name__ == "__main__":
