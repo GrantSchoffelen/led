89ad82c0cec2068efd7e23fa67d1b311e95ae97a
Macbook pro <Home@Macbooks-MacBook-Pro.local>
2017-05-21 21:42:06 -0400

    test

---
+++ rpi-rgb-led-matrix/python/samples/temp.py
@@ -0,0 +1,55 @@
+# from samplebase import SampleBase
+# from rgbmatrix import graphics
+import requests
+import json
+import math
+
+icons = {
+    'clear-day' : 'clear-day',
+    'clear-night': 'clear-night',
+    'rain':'rain',
+    'snow':'snow',
+    'sleet': 'sleet',
+    'wind': 'wind',
+    'fog' : 'fog',
+    'cloudy': 'cloudy',
+    'partly-cloudy-day': 'partly-cloudy-day',
+    'partly-cloudy-night': 'partly-cloudy-night'
+}
+
+while True:
+    r = requests.get('https://api.darksky.net/forecast/80898f5e97e9c9ee461ecf0f94643a4f/40.783428,-73.966248')
+    res = json.loads(r.text)
+    current_weather = icons[res['currently']['icon']]
+    future_8 = icons[res['hourly']['data'][7]['icon']]
+    future_16 = icons[res['hourly']['data'][15]['icon']]
+
+    current_temp = math.ceil(res['currently']['apparentTemperature'])
+
+    print('%d°F' % currentTemp)
+
+
+
+
+#
+# class GraphicsTest(SampleBase):
+#     def __init__(self, *args, **kwargs):
+#         super(GraphicsTest, self).__init__(*args, **kwargs)
+#
+#     def run(self):
+#         canvas = self.matrix
+#         font = graphics.Font()
+#         font.LoadFont("../../fonts/5x8.bdf")
+#         color = graphics.Color(255, 0, 255)
+#
+#         while True:
+#             self.matrix.Clear()
+#             txtTime = datetime.now(eastern).strftime('%l:%Mp')
+#             graphics.DrawText(canvas, font, 0, 6, color, txtTime)
+#             time.sleep(60)
+#
+# # Main function
+# if __name__ == "__main__":
+#     graphics_test = GraphicsTest()
+#     if (not graphics_test.process()):
+#         graphics_test.print_help()
