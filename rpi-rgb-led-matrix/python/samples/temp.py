f5fb711f6217ee027a5d0461dd8acce1680feb42
Macbook pro <Home@Macbooks-MacBook-Pro.local>
2017-11-30 00:13:05 -0500

    temp

--- rpi-rgb-led-matrix/python/samples/temp.py
+++ rpi-rgb-led-matrix/python/samples/temp.py
@@ -1,42 +1,43 @@
+#!/usr/bin/env python
 from samplebase import SampleBase
 from rgbmatrix import graphics
-# import requests
-# import json
-# import math
-#
-# icons = {
-#     "clear-day" : "clear-day",
-#     "clear-night": "clear-night",
-#     "rain":"rain",
-#     "snow":"snow",
-#     "sleet": "sleet",
-#     "wind": "wind",
-#     "fog" : "fog",
-#     "cloudy": "cloudy",
-#     "partly-cloudy-day": "partly-cloudy-day",
-#     "partly-cloudy-night": "partly-cloudy-night"
-# }
+import requests
+import json
+import math
+
+icons = {
+    'clear-day' : 'clear-day',
+    'clear-night': 'clear-night',
+    'rain':'rain',
+    'snow':'snow',
+    'sleet': 'sleet',
+    'wind': 'wind',
+    'fog' : 'fog',
+    'cloudy': 'cloudy',
+    'partly-cloudy-day': 'partly-cloudy-day',
+    'partly-cloudy-night': 'partly-cloudy-night'
+}
 
 class GraphicsTest(SampleBase):
     def __init__(self, *args, **kwargs):
         super(GraphicsTest, self).__init__(*args, **kwargs)
 
     def run(self):
-        # r = requests.get('https://api.darksky.net/forecast/80898f5e97e9c9ee461ecf0f94643a4f/40.783428,-73.966248')
-        # res = json.loads(r.text)
-        # current_weather = icons[res['currently']['icon']]
-        # future_8 = icons[res['hourly']['data'][7]['icon']]
-        # future_16 = icons[res['hourly']['data'][15]['icon']]
-        # current_temp = math.ceil(res['currently']['apparentTemperature'])
-        # textTemp = ('%d°F, %s' % (current_temp, current_weather) )
-        # canvas = self.matrix
-        # font = graphics.Font()
-        # font.LoadFont("../../fonts/5x8.bdf")
-        # color = graphics.Color(255, 0, 255)
-        textTemp = 'hello'
+        r = requests.get('https://api.darksky.net/forecast/80898f5e97e9c9ee461ecf0f94643a4f/40.783428,-73.966248')
+        res = json.loads(r.text)
+        current_weather = icons[res['currently']['icon']]
+        future_8 = icons[res['hourly']['data'][7]['icon']]
+        future_16 = icons[res['hourly']['data'][15]['icon']]
+        current_temp = math.ceil(res['currently']['apparentTemperature'])
+        textTemp = ('%d°F, %s' % (current_temp, current_weather) )
+        canvas = self.matrix
+        font = graphics.Font()
+        font.LoadFont("../../fonts/5x8.bdf")
+        color = graphics.Color(255, 0, 255)
 
         while True:
             self.matrix.Clear()
+
             graphics.DrawText(canvas, font, 0, 6, color, txtTemp)
             time.sleep(60)
 
